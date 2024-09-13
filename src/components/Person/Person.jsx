60729f9e39c7138eec5fae8818b24c41a503911f
Oleksandra Kuziura <radioday2014@gmail.com>
2024-09-13 13:14:04 +0300

    fix apperence of age paragraph

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -3,12 +3,11 @@ import { Partner } from '../Partner';
 export const Person = ({ person }) => (
   <section className="Person">
     <h2 className="Person__name">My name is {person.name}</h2>
-    <p
-      className="Person__age"
-      style={{ display: person.age === undefined ? 'none' : 'block' }}
-    >
-      I am {person.age}
-    </p>
+    {person.age !== undefined ? (
+      <p className="Person__age">I am {person.age}</p>
+    ) : (
+      ''
+    )}
     <Partner
       sex={person.sex}
       isMarried={person.isMarried}
