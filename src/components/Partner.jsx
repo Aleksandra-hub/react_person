1c60b88aa0f03c1ac1a26604b313c2aa3bb9d7f4
Oleksandra Kuziura <radioday2014@gmail.com>
2024-09-14 14:15:16 +0300

    add destruction to the person object

--- src/components/Partner.jsx
+++ src/components/Partner.jsx
@@ -1,11 +1,11 @@
 export const Partner = ({ sex, isMarried, partnerName }) => {
   let message = 'I am not married';
 
-  if (sex === 'm' && isMarried === true) {
+  if (sex === 'm' && isMarried) {
     message = `${partnerName} is my wife`;
   }
 
-  if (sex === 'f' && isMarried === true) {
+  if (sex === 'f' && isMarried) {
     message = `${partnerName} is my husband`;
   }
 
--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,17 +1,13 @@
 import { Partner } from '../Partner';
 
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">My name is {person.name}</h2>
-    {person.age !== undefined ? (
-      <p className="Person__age">I am {person.age}</p>
-    ) : (
-      ''
-    )}
-    <Partner
-      sex={person.sex}
-      isMarried={person.isMarried}
-      partnerName={person.partnerName}
-    />
-  </section>
-);
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+      {age !== undefined ? <p className="Person__age">I am {age}</p> : ''}
+      <Partner sex={sex} isMarried={isMarried} partnerName={partnerName} />
+    </section>
+  );
+};
